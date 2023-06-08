9f60e99cb17d8902a88aaa7ebb4ce51fbf8225d7
ALDBANDA <alfred.banda1986@gmail.com>
2023-06-08 19:07:06 +0100

    'Updates"

--- finovo/src/components/Sidebar.jsx
+++ finovo/src/components/Sidebar.jsx
@@ -7,24 +7,24 @@ const Sidebar = () => {
   const [showBranchSubMenu, setShowBranchSubMenu] = useState(false);
 
   const handleBudgetSubMenuToggle = () => {
-    setShowBudgetSubMenu(!showBudgetSubMenu);
+    setShowBudgetSubMenu(prevState => !prevState);
   };
 
   const handleFinancialSubMenuToggle = () => {
-    setShowFinancialSubMenu(!showFinancialSubMenu);
+    setShowFinancialSubMenu(prevState => !prevState);
   };
 
   const handleBranchSubMenuToggle = () => {
-    setShowBranchSubMenu(!showBranchSubMenu);
+    setShowBranchSubMenu(prevState => !prevState);
   };
 
   return (
     <div className='bg-[#4E73DF] h-screen px-[25px]'>
-      <div className='px-[15px] py-[30px] flex items-centre justify-center border-b-[1px] border-[#EDEDED]/[0.3]'>
-        <h1 className='text-white text-[20px] leading-[24px] font-extrabold cursor-pointer'>Admin Panel</h1>
+      <div className='px-[15px] py-[30px] flex items-center justify-center border-b-[1px] border-[#EDEDED]/[0.3]'>
+        <h1 className='text-white text-[20px] leading-[24px] font-extrabold cursor-pointer'>Finovo Admin Panel</h1>
       </div>
 
-      <div className='flex-items gap-[15px] py-[20px] border-b-[1px] border-[#EDEDED]/[0.3]'>
+      <div className='flex items-center gap-[15px] py-[20px] border-b-[1px] border-[#EDEDED]/[0.3]'>
         <FaTachometerAlt color='white' />
         <p className='text-[14px] leading-[20px] font-bold text-white'>Dashboard</p>
       </div>
@@ -32,11 +32,10 @@ const Sidebar = () => {
       <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
         <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'>INTERFACE</p>
 
-
         <div className='flex items-center justify-between gap-[10px] py-[15px] cursor-pointer' onClick={handleBudgetSubMenuToggle}>
           <div className='flex items-center gap-[10px]'>
             <FaRegSun color='white' />
-            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300 '>Budget Management</p>
+            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300'>Budget Management</p>
           </div>
           <FaChevronRight color='white' className={showBudgetSubMenu ? 'transform rotate-90' : ''} />
         </div>
@@ -51,34 +50,32 @@ const Sidebar = () => {
           </div>
         )}
 
-
         <div className='flex items-center justify-between gap-[10px] py-[15px] cursor-pointer' onClick={handleFinancialSubMenuToggle}>
           <div className='flex items-center gap-[10px]'>
             <FaWallet color='white' />
-            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300 '>Financial Tools</p>
+            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300'>Financial Tools</p>
           </div>
           <FaChevronRight color='white' className={showFinancialSubMenu ? 'transform rotate-90' : ''} />
         </div>
         {showFinancialSubMenu && (
           <div className='pl-[20px]'>
-           <ul className='list-disc p1-4 text-white'>
+            <ul className='list-disc p1-4 text-white'>
               <li className='hover:text-blue-300 cursor-pointer'>Loan Calculator</li>
               <li className='hover:text-blue-300 cursor-pointer'>Savings Goal Tracker</li>
             </ul>
           </div>
         )}
 
-
         <div className='flex items-center justify-between gap-[10px] py-[15px] cursor-pointer' onClick={handleBranchSubMenuToggle}>
           <div className='flex items-center gap-[10px]'>
             <FaBuilding color='white' />
-            <p className='text-[14px] leading-[20px] font-normal hover:text-blue-300  text-white'>Branch & Services</p>
+            <p className='text-[14px] leading-[20px] font-normal hover:text-blue-300 text-white'>Branch & Services</p>
           </div>
           <FaChevronRight color='white' className={showBranchSubMenu ? 'transform rotate-90' : ''} />
         </div>
         {showBranchSubMenu && (
           <div className='pl-[20px]'>
-         <ul className='list-disc p1-4 text-white'>
+            <ul className='list-disc p1-4 text-white'>
               <li className='hover:text-blue-300 cursor-pointer'>Branch Locator</li>
               <li className='hover:text-blue-300 cursor-pointer'>Financial Education</li>
             </ul>
@@ -88,9 +85,8 @@ const Sidebar = () => {
         <div className='flex items-center justify-between gap-[10px] py-[15px] cursor-pointer'>
           <div className='flex items-center gap-[10px]'>
             <FaHeadset color='white' />
-            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300  cursor-pointer'>Customer Support</p>
+            <p className='text-[14px] leading-[20px] font-normal text-white hover:text-blue-300'>Customer Support</p>
           </div>
-        
         </div>
 
       </div>
