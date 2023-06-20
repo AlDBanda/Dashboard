2dac7b6a1067b9c51ffcd90b1a508c68da7e0696
ALDBANDA <alfred.banda1986@gmail.com>
2023-06-20 21:10:25 +0100

    'Budget planner updated'

--- finovo/src/Pages/BudgetPlanner.jsx
+++ finovo/src/Pages/BudgetPlanner.jsx
@@ -1,98 +1,156 @@
 import React, { useState } from 'react';
-import { FaPlus, FaTrash } from 'react-icons/fa';
+
 
 const BudgetPlanner = () => {
-  const [transactions, setTransactions] = useState([]);
-  const [newTransactionName, setNewTransactionName] = useState('');
-  const [newTransactionAmount, setNewTransactionAmount] = useState('');
+  const [income, setIncome] = useState(0);
+  const [housing, setHousing] = useState(0);
+  const [insurance, setInsurance] = useState(0);
+  const [groceries, setGroceries] = useState(0);
+  const [media, setMedia] = useState(0);
+  const [debtRepayment, setDebtRepayment] = useState(0);
+  const [otherEssentials, setOtherEssentials] = useState(0);
+  const [lifestyleCosts, setLifestyleCosts] = useState(0);
+  const [savings, setSavings] = useState(0);
 
-  const handleAddTransaction = () => {
-    if (newTransactionName !== '' && newTransactionAmount !== '') {
-      const newTransaction = {
-        name: newTransactionName,
-        amount: parseFloat(newTransactionAmount),
-      };
-
-      const updatedTransactions = [...transactions, newTransaction];
-      setTransactions(updatedTransactions);
-
-      setNewTransactionName('');
-      setNewTransactionAmount('');
-    }
+  const calculateTotalOutgoings = () => {
+    return (
+      housing +
+      insurance +
+      groceries +
+      media +
+      debtRepayment +
+      otherEssentials +
+      lifestyleCosts
+    );
   };
 
-  const handleDeleteTransaction = (index) => {
-    const updatedTransactions = [...transactions];
-    updatedTransactions.splice(index, 1);
-    setTransactions(updatedTransactions);
+  const calculateMoneyLeftover = () => {
+    return income - calculateTotalOutgoings();
   };
 
-  const calculateTotal = () => {
-    return transactions.reduce((total, transaction) => total + transaction.amount, 0);
+  const calculatePercentage = (amount, total) => {
+    return ((amount / total) * 100).toFixed(2);
   };
 
-  const calculateRemainingBudget = () => {
-    const totalSpent = calculateTotal();
-    return totalSpent.toFixed(2);
+  const calculateEssentialSpendPercentage = () => {
+    const essentials = housing + insurance + groceries;
+    return calculatePercentage(essentials, income);
   };
 
-  const calculateBudgetLimit = () => {
-    const budgetLimit = 5000; // Set your budget limit here
-    const remainingBudget = budgetLimit - calculateTotal();
-    return remainingBudget.toFixed(2);
+  const calculateNonEssentialSpendPercentage = () => {
+    const nonEssentials = media + lifestyleCosts;
+    return calculatePercentage(nonEssentials, income);
+  };
+
+  const calculateSavingsPercentage = () => {
+    const savingsPercentage = savings + debtRepayment;
+    return calculatePercentage(savingsPercentage, income);
   };
 
   return (
     <div className="max-w-md mx-auto p-4">
       <h2 className="text-2xl font-bold mb-4">Budget Planner</h2>
 
-      <div className="flex mb-4">
-        <input
-          type="text"
-          className="w-1/2 px-4 py-2 mr-2 rounded-lg border focus:outline-none"
-          placeholder="Enter transaction name"
-          value={newTransactionName}
-          onChange={(e) => setNewTransactionName(e.target.value)}
-        />
+      <div className="mb-4">
+        <label className="block font-bold mb-2">Income after tax:</label>
         <input
           type="number"
-          className="w-1/2 px-4 py-2 ml-2 rounded-lg border focus:outline-none"
-          placeholder="Enter transaction amount"
-          value={newTransactionAmount}
-          onChange={(e) => setNewTransactionAmount(e.target.value)}
+          className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+          value={income}
+          onChange={(e) => setIncome(parseFloat(e.target.value))}
         />
-        <button
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
-          onClick={handleAddTransaction}
-        >
-          <FaPlus />
-        </button>
       </div>
 
-      <ul className="space-y-2">
-        {transactions.map((transaction, index) => (
-          <li
-            key={index}
-            className="flex items-center justify-between px-4 py-2 bg-gray-100 rounded-lg"
-          >
-            <span>{transaction.name}</span>
-            <span>${transaction.amount}</span>
-            <button
-              className="text-red-500 hover:text-red-600 focus:outline-none"
-              onClick={() => handleDeleteTransaction(index)}
-            >
-              <FaTrash />
-            </button>
-          </li>
-        ))}
-      </ul>
+      <h3 className="text-lg font-bold mb-2">Expenses</h3>
+
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div>
+          <label className="block font-bold mb-2">Housing:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={housing}
+            onChange={(e) => setHousing(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Insurance:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={insurance}
+            onChange={(e) => setInsurance(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Groceries:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={groceries}
+            onChange={(e) => setGroceries(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Media and Entertainment:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={media}
+            onChange={(e) => setMedia(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Debt Repayment:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={debtRepayment}
+            onChange={(e) => setDebtRepayment(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Other Essentials:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={otherEssentials}
+            onChange={(e) => setOtherEssentials(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Lifestyle Costs:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={lifestyleCosts}
+            onChange={(e) => setLifestyleCosts(parseFloat(e.target.value))}
+          />
+        </div>
+        <div>
+          <label className="block font-bold mb-2">Savings:</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg border focus:outline-none"
+            value={savings}
+            onChange={(e) => setSavings(parseFloat(e.target.value))}
+          />
+        </div>
+      </div>
 
       <div className="mt-4">
-        <p className="text-lg font-bold">Total: £{calculateTotal()}</p>
-        <p className="text-lg font-bold">Remaining Budget: £{calculateBudgetLimit()}</p>
+        <h3 className="text-lg font-bold mb-2">Summary</h3>
+        <p>Total Income: £{income}</p>
+        <p>Total Outgoings: £{calculateTotalOutgoings()}</p>
+        <p>Money Leftover: £{calculateMoneyLeftover()}</p>
+        <p>Essential Spend: {calculateEssentialSpendPercentage()}%</p>
+        <p>Non-Essential Spend: {calculateNonEssentialSpendPercentage()}%</p>
+        <p>Savings: {calculateSavingsPercentage()}%</p>
       </div>
     </div>
   );
 };
+
+
 
 export default BudgetPlanner;
